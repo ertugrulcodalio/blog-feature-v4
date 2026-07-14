afc07b27c17566c34a95a133bb863c825f6a66af
Ertuğrul <erto.14.95@gmail.com>
2026-07-14 00:20:24 -0400

    fronend added

--- app/policies/blog_policy.rb
+++ app/policies/blog_policy.rb
@@ -4,18 +4,18 @@ class BlogPolicy < Rhino::ResourcePolicy
   self.resource_slug = 'blogs'
 
   def permitted_attributes_for_show(user)
-    return ['*'] if has_role?(user, 'admin') || has_role?(user, 'user')
+    ['*']
+  end
+
+  def hidden_attributes_for_show(user)
     []
   end
-def hidden_attributes_for_show(user)
-  []
-end
+
   def permitted_attributes_for_create(user)
-    return ['*'] if has_role?(user, 'admin')
-    []
+    ['*']
   end
+
   def permitted_attributes_for_update(user)
-    return ['*'] if has_role?(user, 'admin')
-    []
+    ['*']
   end
 end
